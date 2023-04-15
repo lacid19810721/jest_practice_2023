72d6a8e32a5ddbe2b9cdec79be383e7467408b15
lacid00 <lacid00@naver.com>
2023-04-15 22:53:32 +0900

    Main changed test

--- drink.test.js
+++ drink.test.js
@@ -4,19 +4,19 @@ function drinkAll(callback, flavour) {
     }
   }
   
-  describe('drinkAll', () => {
-  
-    test('drinks something lemon-flavoured', () => {
-      const drink = jest.fn();
-      drinkAll(drink, 'lemon');
-      
-      expect(drink).toHaveBeenCalled(); // 함수 호출 O
-    });
-  
-    test('does not drink something octopus-flavoured', () => {
-      const drink = jest.fn();
-      drinkAll(drink, 'octopus');
-      
-      expect(drink).not.toHaveBeenCalled(); // 함수 호출 X
-    });
+describe('drinkAll', () => {
+
+  test('drinks something lemon-flavoured', () => {
+    const drink = jest.fn();
+    drinkAll(drink, 'lemon');
+    
+    expect(drink).toHaveBeenCalled(); // 함수 호출 O
   });
+
+  test('does not drink something octopus-flavoured', () => {
+    const drink = jest.fn();
+    drinkAll(drink, 'octopus');
+    
+    expect(drink).not.toHaveBeenCalled(); // 함수 호출 X
+  });
+});
